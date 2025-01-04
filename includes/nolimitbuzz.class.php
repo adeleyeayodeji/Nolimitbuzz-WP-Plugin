d935a2fe4bc6c16a6cbdd32fe4c0f15ce058588c
adeleyeayodeji <biodunhi@gmail.com>
2025-01-04 11:14:15 +0100

    Added update

---
+++ includes/nolimitbuzz.class.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Nolimitbuzz\Includes;
+
+/**
+ * Nolimitbuzz class
+ *
+ * @package Nolimitbuzz
+ */
+
+//check for security
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Nolimitbuzz
+{
+    /**
+     * Instance
+     * 
+     * @var Nolimitbuzz
+     */
+    private static $instance;
+
+    /**
+     * Constructor
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        //register post types
+        add_action('init', array($this, 'register_post_types'));
+        //register taxonomies
+        add_action('init', array($this, 'register_taxonomies'));
+        //register shortcode
+        add_shortcode('portfolio', array($this, 'portfolio_shortcode'));
+        //enqueue styles
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
+    }
+
+    /**
+     * Get instance
+     * 
+     * @return Nolimitbuzz
+     */
+    public static function get_instance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Enqueue styles
+     * 
+     * @return void
+     */
+    public function enqueue_styles()
+    {
+        //enqueue styles for the plugin
+        wp_enqueue_style('nolimitbuzz-style', NO_LIMIT_BUZZ_PLUGIN_URL . 'assets/css/style.css', array(), NO_LIMIT_BUZZ_PLUGIN_VERSION, 'all');
+    }
+
+    /**
+     * Register post types
+     * 
+     * @return void
+     */
+    public function register_post_types()
+    {
+        //register portfolio post type
+        register_post_type('portfolio', array(
+            'labels' => array(
+                'name' => __('Portfolios'),
+                'singular_name' => __('Portfolio')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'rewrite' => array('slug' => 'portfolios'),
+        ));
+    }
+
+    /**
+     * Register taxonomies for portfolios
+     * 
+     * @return void
+     */
+    public function register_taxonomies()
+    {
+        //register portfolio category taxonomy
+        register_taxonomy('portfolio_category', 'portfolio', array(
+            'labels' => array(
+                'name' => __('Portfolio Categories'),
+                'singular_name' => __('Portfolio Category')
+            ),
+            'hierarchical' => true,
+            'show_ui' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+            'rewrite' => array('slug' => 'portfolio-category'),
+        ));
+    }
+
+    /**
+     * Portfolio shortcode
+     * 
+     * @return string
+     */
+    // ... existing code ...
+    public function portfolio_shortcode()
+    {
+        //args for the query
+        $args = array(
+            'post_type' => 'portfolio',
+            'posts_per_page' => -1,
+            'orderby' => 'date',
+            'order' => 'DESC'
+        );
+
+        //get all portfolios
+        $query = new \WP_Query($args);
+        $output = '';
+
+        //check if there are any portfolios
+        if ($query->have_posts()) {
+            //get all portfolio categories
+            $terms = get_terms(array(
+                'taxonomy' => 'portfolio_category',
+                'hide_empty' => false,
+            ));
+
+            //check if there are any portfolio categories
+            if ($terms) {
+                //loop through portfolio categories
+                foreach ($terms as $term) {
+                    $output .= '<div class="portfolio-group">';
+                    $output .= '<h2>' . esc_html($term->name) . '</h2>';
+                    $output .= '<ul>';
+
+                    //loop through portfolios
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        if (has_term($term->term_id, 'portfolio_category')) {
+                            $output .= '<li>' . get_the_title() . '</li>';
+                        }
+                    }
+
+                    $output .= '</ul>';
+                    $output .= '</div>';
+                    //reset post data
+                    wp_reset_postdata();
+                }
+            }
+        }
+
+        //check if empty
+        if (empty($output)) {
+            $output .= '<p>' . __('No portfolios found', 'nolimitbuzz') . '</p>';
+        }
+
+        return $output;
+    }
+}
---
+++ nolimitbuzz.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: Nolimitbuzz
+ * Plugin URI:  https://nolimitbuzz.com
+ * Author:      Nolimitbuzz
+ * Author URI:  https://nolimitbuzz.com
+ * Description: This plugin extends the functionality of the Nolimitbuzz theme.
+ * Version:     0.1.0
+ * License:     GPL-2.0+
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: nolimitbuzz
+ */
+
+//check for security
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+//define constants
+define('NO_LIMIT_BUZZ_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('NO_LIMIT_BUZZ_PLUGIN_URL', plugin_dir_url(__FILE__));
+//version
+define('NO_LIMIT_BUZZ_PLUGIN_VERSION', time());
+
+//load the plugin
+require_once NO_LIMIT_BUZZ_PLUGIN_DIR . 'includes/nolimitbuzz.class.php';
+
+//initialize the plugin
+Nolimitbuzz\Includes\Nolimitbuzz::get_instance();
